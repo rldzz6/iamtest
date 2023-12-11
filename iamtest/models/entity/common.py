00d5ba5631c53e1b462d0e042fa35f3e07066fc3
rldzz6 <149875114+rldzz6@users.noreply.github.com>
2023-12-11 21:13:57 +0900

    Update common.py

--- iamtest/models/entity/common.py
+++ iamtest/models/entity/common.py
@@ -2,17 +2,26 @@ from pydantic import BaseModel
 from typing import Dict, Any, List
 from datetime import datetime
 
-class response(BaseModel):
+class Response(BaseModel):
     code: str | None = ''
     message: str | None = ''
-    data: List[dict[str, Any]] | None = []
+    data: List[Dict[str, Any]] | Dict[str, Any] | None = None
+    total_count: int | None = 0
+    total_page: int | None = 1
 
 class Log(BaseModel):
-    id: int | None = None
-    description: str | None = None
+    log_id: int | None = None
     category: str | None = None
-    work_type: str | None = None
-    work_time: str | None = str(datetime.now())
+    description: str | None = None
+    action: str | None = None
+    action_time: str | None = None
     employee_id: str | None = None
-    work_ip: str | None = None
-    work_status: int | None = None
+    ip: str | None = None
+
+class Errorlog(BaseModel):
+    identity: str | None = None
+    url_path: str | None = None
+    asc_time: str | None = None
+    status: str | None = None
+    level: str | None = None
+    message: str | None = ''
